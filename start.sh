a0fcc24cce2c991df5f1861cf8d48bda4eb1c061
Mark Kershaw <mark.kershaw@mambu.com>
2019-04-23 08:50:14 +0100

    update

--- start.sh
+++ start.sh
@@ -6,3 +6,12 @@ cd ${DIR}
 
 source ../env/bin/activate
 python syncWebServer.py http://ec2-52-56-214-201.eu-west-2.compute.amazonaws.com:8001&
+VIBORA_PID=$!
+
+# Create a stop.sh script to kill the running instances
+echo "#!/bin/bash" > stop.sh
+chmod +x stop.sh
+echo "# this script is automatically created - from start.sh"
+echo "kill -9 ${VIBORA_PID}" >> stop.sh
+echo "***********************"
+echo "run ${DIR}/stop.sh to stop the servers"
