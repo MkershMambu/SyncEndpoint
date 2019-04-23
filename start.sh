83a34da00f30bc7bc29553d30a930d01666ce072
Mark Kershaw <mark.kershaw@mambu.com>
2019-04-23 08:37:50 +0100

    update

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# !/bin/bash -x to turn echo on
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd ${DIR}/..
+
+source env/bin/activate
+python synvWebServer.py http://ec2-52-56-214-201.eu-west-2.compute.amazonaws.com:8001&
